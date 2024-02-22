8eadf7b3e53bdcb9fad7939ea8608b7d5757d6be
Rafael Andrade <rafaandrade2810@gmail.com>
2024-02-22 03:16:06 -0300

    Refatorando para separação do Converter através da classe NumberConverter

--- src/main/java/br/com/randrade/controller/MathController.java
+++ src/main/java/br/com/randrade/controller/MathController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.randrade.converter.NumberConverter;
 import br.com.randrade.exceptions.UnsupportedMathOperationException;
 
 /**
@@ -28,24 +29,11 @@ public class MathController {
 			@PathVariable(value = "numberTwo") String numberTwo
 			) throws Exception {
 		
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationException("Please set a numeric value!");
 		}
 		
-		return convertToDouble(numberOne) + convertToDouble(numberTwo);
-	}
-
-	private Double convertToDouble(String strNumber) {
-		if (strNumber == null) return 0D;
-		String number = strNumber.replaceAll(",", ".");
-		if (isNumeric(number)) return Double.parseDouble(number);
-		return 0D;
-	}
-
-	private boolean isNumeric(String strNumber) {
-		if (strNumber == null) return false;
-		String number = strNumber.replaceAll(",", ".");
-		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+		return (NumberConverter.convertToDouble(numberOne)) + (NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	//SUBTRAÇÃO
@@ -56,11 +44,11 @@ public class MathController {
 			@PathVariable(value = "numberTwo") String numberTwo
 			) throws Exception {
 		
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationException("Please set a numeric value!");
 		}
 		
-		return convertToDouble(numberOne) - convertToDouble(numberTwo);
+		return (NumberConverter.convertToDouble(numberOne)) - (NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	//MULTIPLICAÇÃO
@@ -71,11 +59,11 @@ public class MathController {
 			@PathVariable(value = "numberTwo") String numberTwo
 			) throws Exception {
 		
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationException("Please set a numeric value!");
 		}
 		
-		return convertToDouble(numberOne) * convertToDouble(numberTwo);
+		return (NumberConverter.convertToDouble(numberOne)) * (NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	//DIVISÃO
@@ -86,11 +74,11 @@ public class MathController {
 			@PathVariable(value = "numberTwo") String numberTwo
 			) throws Exception {
 		
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationException("Please set a numeric value!");
 		}
 		
-		return convertToDouble(numberOne) / convertToDouble(numberTwo);
+		return (NumberConverter.convertToDouble(numberOne)) / (NumberConverter.convertToDouble(numberTwo));
 	}
 	
 	//MÉDIA
@@ -101,11 +89,11 @@ public class MathController {
 			@PathVariable(value = "numberTwo") String numberTwo
 			) throws Exception {
 		
-		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+		if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
 			throw new UnsupportedMathOperationException("Please set a numeric value!");
 		}
 		
-		return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+		return ((NumberConverter.convertToDouble(numberOne)) + (NumberConverter.convertToDouble(numberTwo))) / 2;
 	}
 	
 }
